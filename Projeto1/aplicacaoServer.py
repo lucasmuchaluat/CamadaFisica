cda03f2bb1cf2f8b6dcf191d0c0d59bff995ae37
Lucas Muchaluat <lucasmuchaluat@gmail.com>
2019-08-14 15:36:56 -0300

    projeto 1 client/server

---
+++ Projeto1/aplicacaoServer.py
@@ -0,0 +1,105 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#####################################################
+# Camada Física da Computação
+#Carareto
+#17/02/2018
+#  Aplicação 
+####################################################
+
+print("comecou")
+
+from enlace import *
+import time
+
+
+# Serial Com Port
+#   para saber a sua porta, execute no terminal :
+#   python -m serial.tools.list_ports
+
+#serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
+#serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
+serialName = "COM5"                  # Windows(variacao de)
+print("abriu com")
+
+def main():
+    # Inicializa enlace ... variavel com possui todos os metodos e propriedades do enlace, que funciona em threading
+    com = enlace(serialName) # repare que o metodo construtor recebe um string (nome)
+    # Ativa comunicacao
+    com.enable()
+
+   
+
+    # Log
+    print("-------------------------")
+    print("Comunicação inicializada")
+    print("  porta : {}".format(com.fisica.name))
+    print("-------------------------")
+
+    # Carrega dados
+    print ("gerando dados para transmissao :")
+  
+      #no exemplo estamos gerando uma lista de bytes ou dois bytes concatenados
+    
+    #exemplo 1
+    #ListTxBuffer =list()
+    #for x in range(1,10):
+    #    ListTxBuffer.append(x)
+    #txBuffer = bytes(ListTxBuffer)
+    
+    #exemplo2
+    #txBuffer = bytes([2]) + bytes([3])+ bytes("teste", 'utf-8')
+    with open ("insper.jpg", "rb") as img:
+      img = img.read()
+      txBuffer = bytearray(img)
+
+    
+    
+    txLen    = len(txBuffer)
+    print(txLen)
+
+    txLen_bytes = txLen.to_bytes(length=2,byteorder='big')
+    # barra = bytearray(b'barra')
+    data = txLen_bytes + txBuffer # + barra 
+
+    
+    # Transmite dado
+    print("tentado transmitir .... {} bytes".format(txLen))
+    com.sendData(data)
+
+    # espera o fim da transmissão
+    while(com.tx.getIsBussy()):
+       pass
+    
+    
+    # Atualiza dados da transmissão
+    txSize = com.tx.getStatus()
+    print ("Transmitido       {} bytes ".format(txSize))
+
+    # Faz a recepção dos dados
+    # print ("Recebendo dados .... ")
+    
+    #repare que o tamanho da mensagem a ser lida é conhecida!     
+    # rxBuffer, nRx = com.getData(txLen)
+
+    # log
+    # print ("Lido              {} bytes ".format(nRx))
+    
+    # print (rxBuffer)
+    print("Buffer transmitido para o outro Arduino")
+
+    with open ("insper_after.jpg", "wb") as img_final:
+      img_final.write(txBuffer)
+
+    print(txBuffer)
+
+    # Encerra comunicação
+    print("-------------------------")
+    print("Comunicação encerrada")
+    print("-------------------------")
+    com.disable()
+
+    #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
+if __name__ == "__main__":
+    main()
---
+++ Projeto1/enlaceTx.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#####################################################
+# Camada Física da Computação
+#Carareto
+#08/008/2019
+#  Camada de Enlace 
+####################################################
+
+# Importa pacote de tempo
+import time
+# Threads
+import threading
+
+# Class
+class TX(object):
+    """ This class implements methods to handle the transmission
+        data over the p2p fox protocol
+    """
+
+    def __init__(self, fisica):
+        """ Initializes the TX class
+        """
+        self.fisica      = fisica
+        self.buffer      = bytes(bytearray())
+        self.transLen    = 0
+        self.empty       = True
+        self.threadMutex = False
+        self.threadStop  = False
+
+
+    def thread(self):
+        """ TX thread, to send data in parallel with the code
+        """
+        while not self.threadStop:
+            if(self.threadMutex):
+                self.transLen    = self.fisica.write(self.buffer)
+                print("O tamanho transmitido. IMpressao dentro do thread {}" .format(self.transLen))
+                self.threadMutex = False
+
+    def threadStart(self):
+        """ Starts TX thread (generate and run)
+        """
+        self.thread = threading.Thread(target=self.thread, args=())
+        self.thread.start()
+
+    def threadKill(self):
+        """ Kill TX thread
+        """
+        self.threadStop = True
+
+    def threadPause(self):
+        """ Stops the TX thread to run
+
+        This must be used when manipulating the tx buffer
+        """
+        self.threadMutex = False
+
+    def threadResume(self):
+        """ Resume the TX thread (after suspended)
+        """
+        self.threadMutex = True
+
+    def sendBuffer(self, data):
+        """ Write a new data to the transmission buffer.
+            This function is non blocked.
+
+        This function must be called only after the end
+        of transmission, this erase all content of the buffer
+        in order to save the new value.
+        """
+        self.transLen   = 0
+        self.buffer = data
+        self.threadMutex  = True
+
+    def getBufferLen(self):
+        """ Return the total size of bytes in the TX buffer
+        """
+        return(len(self.buffer))
+
+    def getStatus(self):
+        """ Return the last transmission size
+        """
+        print("O tamanho transmitido (Impressao fora do thread)  {}" .format(self.transLen))
+        return(self.transLen)
+        
+
+    def getIsBussy(self):
+        """ Return true if a transmission is ongoing
+        """
+        return(self.threadMutex)
+
