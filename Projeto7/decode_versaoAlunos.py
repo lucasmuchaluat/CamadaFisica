57c544fb55067bd0c4b059cee493a601c50fc41b
Lucas Muchaluat <lucasmuchaluat@gmail.com>
2019-10-11 18:21:49 -0300

    Projeto 7 - DTMF

---
+++ Projeto7/decode_versaoAlunos.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+"""Show a text-mode spectrogram using live microphone data."""
+
+#Importe todas as bibliotecas
+from suaBibSignal import *
+import numpy as np
+import sounddevice as sd
+import matplotlib.pyplot as plt
+import time
+import pickle
+import math
+import peakutils
+import itertools
+
+#funcao para transformas intensidade acustica em dB
+def todB(s):
+    sdB = 10*np.log10(s)
+    return(sdB)
+
+def oneDArray(x):
+    return list(itertools.chain(*x))
+
+
+def main():
+ 
+    #declare um objeto da classe da sua biblioteca de apoio (cedida)    
+    signal = signalMeu()
+    #declare uma variavel com a frequencia de amostragem, sendo 44100
+    freqDeAmostragem = 44100
+    #voce importou a bilioteca sounddevice como, por exemplo, sd. entao
+    # os seguintes parametros devem ser setados:
+    
+    sd.default.samplerate = 44100 #taxa de amostragem
+    sd.default.channels = 2  #voce pode ter que alterar isso dependendo da sua placa
+    duration = 3 #tempo em segundos que ira aquisitar o sinal acustico captado pelo mic
+    n = 2
+
+    # faca um printo na tela dizendo que a captacao comecará em n segundos. e entao 
+    #use um time.sleep para a espera
+
+    print("Iniciando captação do áudio em {} segundos.".format(n))
+    time.sleep(2)
+    
+   
+    #faca um print informando que a gravacao foi inicializada
+
+    print("Gravação iniciada!")
+   
+    #declare uma variavel "duracao" com a duracao em segundos da gravacao. poucos segundos ... 
+    #calcule o numero de amostras "numAmostras" que serao feitas (numero de aquisicoes)
+   
+    
+    
+    numAmostras = freqDeAmostragem * duration
+   
+    audio = sd.rec(int(numAmostras), freqDeAmostragem, channels=1)
+    sd.wait()
+    print("...     FIM")
+    
+    #analise sua variavel "audio". pode ser um vetor com 1 ou 2 colunas, lista ...
+    #grave uma variavel com apenas a parte que interessa (dados)
+    
+    audio_list = oneDArray(audio)
+    
+    
+
+    # use a funcao linspace e crie o vetor tempo. Um instante correspondente a cada amostra!
+    inicio = 0
+    fim = 3 
+    numPontos = len(audio_list)
+    t = np.linspace(inicio,fim,numPontos)
+
+    # plot do gravico  áudio vs tempo!
+
+    plt.plot(audio_list, t)
+    plt.xlabel('Tempo')
+    plt.ylabel('Áudio')
+    plt.show()
+    
+   
+    
+    # Calcula e exibe o Fourier do sinal audio. como saida tem-se a amplitude e as frequencias
+    xf, yf = signal.calcFFT(audio_list, sd.default.samplerate)
+    plt.figure("F(y)")
+    plt.plot(xf,yf)
+    plt.grid()
+    plt.title('Fourier audio')
+    
+
+    #esta funcao analisa o fourier e encontra os picos
+    #voce deve aprender a usa-la. ha como ajustar a sensibilidade, ou seja, o que é um pico?
+    #voce deve tambem evitar que dois picos proximos sejam identificados, pois pequenas variacoes na
+    #frequencia do sinal podem gerar mais de um pico, e na verdade tempos apenas 1.
+   
+    indexes = peakutils.indexes(yf, thres=0.5, min_dist=15)
+    
+    #printe os picos encontrados!
+
+    print(indexes) 
+    print(xf[indexes], yf[indexes])
+    print(indexes[0])
+    
+
+    
+
+
+    def calcFreq(index, freq):
+        dif = math.absolute(freq - index)
+        return dif
+
+    #encontre na tabela duas frequencias proximas às frequencias de pico encontradas e descubra qual foi a tecla
+
+    
+    if calcFreq(indexes[0], 1209) <= 10 and calcFreq(indexes[1], 697) <= 10:
+        char = "1"
+    elif calcFreq(indexes[0], 1336) <= 10 and calcFreq(indexes[1], 697) <= 10:
+        char = "2"
+    elif calcFreq(indexes[0], 1477) <= 10 and calcFreq(indexes[1], 697) <= 10:
+        char = "3"
+    elif calcFreq(indexes[0], 1633) <= 10 and calcFreq(indexes[1], 697) <= 10:
+        char = "A"
+    elif calcFreq(indexes[0], 1209) <= 10 and calcFreq(indexes[1], 770) <= 10:
+        char = "4"
+    elif calcFreq(indexes[0], 1336) <= 10 and calcFreq(indexes[1], 770) <= 10:
+        char = "5"
+    elif calcFreq(indexes[0], 1477) <= 10 and calcFreq(indexes[1], 770) <= 10:
+        char = "6"
+    elif calcFreq(indexes[0], 1633) <= 10 and calcFreq(indexes[1], 770) <= 10:
+        char = "B"
+    elif calcFreq(indexes[0], 1209) <= 10 and calcFreq(indexes[1], 852) <= 10:
+        char = "7"
+    elif calcFreq(indexes[0], 1336) <= 10 and calcFreq(indexes[1], 852) <= 10:
+        char = "8"
+    elif calcFreq(indexes[0], 1477) <= 10 and calcFreq(indexes[1], 852) <= 10:
+        char = "9"
+    elif calcFreq(indexes[0], 1633) <= 10 and calcFreq(indexes[1], 852) <= 10:
+        char = "C"
+    elif calcFreq(indexes[0], 1209) <= 10 and calcFreq(indexes[1], 941) <= 10:
+        char = "X"
+    elif calcFreq(indexes[0], 1336) <= 10 and calcFreq(indexes[1], 941) <= 10:
+        char = "0"
+    elif calcFreq(indexes[0], 1477) <= 10 and calcFreq(indexes[1], 941) <= 10:
+        char = "#"
+    elif calcFreq(indexes[0], 1633) <= 10 and calcFreq(indexes[1], 941) <= 10:
+        char = "D"
+    else:
+        char = "Por favor, repita o procedimento de captação do áudio."
+
+    #print a tecla.
+    print(f"O número captado foi {char}")
+    
+    ## Exibe gráficos
+    plt.show()
+
+if __name__ == "__main__":
+    main()
---
+++ Projeto7/encode_versaoAlunos.py
@@ -0,0 +1,142 @@
+
+
+#importe as bibliotecas
+from suaBibSignal import *
+import numpy as np
+import sounddevice as sd
+import matplotlib.pyplot as plt
+import sys
+
+
+
+def signal_handler(signal, frame):
+        print('You pressed Ctrl+C!')
+        sys.exit(0)
+
+#converte intensidade em Db, caso queiram ...
+def todB(s):
+    sdB = 10*np.log10(s)
+    return(sdB)
+
+def main():
+    print("Inicializando encoder")
+    
+    #declare um objeto da classe da sua biblioteca de apoio (cedida)    
+    signal = signalMeu()
+
+    #declare uma variavel com a frequencia de amostragem, sendo 44100
+    freq_amostra = 44100
+    
+    #voce importou a bilioteca sounddevice como, por exemplo, sd. entao os seguintes parametros devem ser setados:
+    
+    duration = 3 #tempo em segundos que ira emitir o sinal acustico
+
+    #relativo ao volume. Um ganho alto pode saturar sua placa... comece com .3    
+    gainX  = 0.3
+    gainY  = 0.3
+
+
+    print("Gerando Tons base")
+
+    #printe a mensagem para o usuario teclar um numero de 0 a 9. 
+    #nao aceite outro valor de entrada.
+
+    valid_number = False
+    while valid_number == False:
+        char = input("Qual é o simbolo desejado?")
+        if char == "1":
+            freq_1 = 1209
+            freq_2 = 697
+            valid_number = True
+        elif char == "2":
+            freq_1 = 1336
+            freq_2 = 697
+            valid_number = True
+        elif char == "3":
+            freq_1 = 1477
+            freq_2 = 697
+            valid_number = True
+        elif char == "a" or char == "A":
+            freq_1 = 1633
+            freq_2 = 697
+            valid_number = True
+        elif char == "4":
+            freq_1 = 1209
+            freq_2 = 770
+            valid_number = True
+        elif char == "5":
+            freq_1 = 1336
+            freq_2 = 770
+            valid_number = True
+        elif char == "6":
+            freq_1 = 1477
+            freq_2 = 770
+            valid_number = True
+        elif char == "b" or char == "B":
+            freq_1 = 1633
+            freq_2 = 770
+            valid_number = True
+        elif char == "7":
+            freq_1 = 1209
+            freq_2 = 852
+            valid_number = True
+        elif char == "8":
+            freq_1 = 1336
+            freq_2 = 852
+            valid_number = True
+        elif char == "9":
+            freq_1 = 1477
+            freq_2 = 852
+            valid_number = True
+        elif char == "c" or char == "C":
+            freq_1 = 1633
+            freq_2 = 852
+            valid_number = True
+        elif char == "x" or char == "X":
+            freq_1 = 1209
+            freq_2 = 941
+            valid_number = True
+        elif char == "0":
+            freq_1 = 1336
+            freq_2 = 941
+            valid_number = True
+        elif char == "#":
+            freq_1 = 1477
+            freq_2 = 941
+            valid_number = True
+        elif char == "d" or char == "D":
+            freq_1 = 1633
+            freq_2 = 941
+            valid_number = True
+        else:
+            valid_number = False
+
+    #gere duas senoides para cada frequencia da tabela DTMF ! Canal x e canal y 
+    #use para isso sua biblioteca (cedida)
+    #obtenha o vetor tempo tb.
+    #deixe tudo como array
+        
+    tb_x, sen_x = signal.generateSin(freq_1, 0.5, duration, freq_amostra)
+    tb_y, sen_y = signal.generateSin(freq_2, 0.5, duration, freq_amostra)
+
+    print("Gerando Tom referente ao símbolo : {}".format(char))
+    print("Frequencias: {0} + {1}".format(freq_1,freq_2))
+    
+    
+    #construa o sinal a ser reproduzido. nao se esqueca de que é a soma das senoides
+    tone = sen_x + sen_y
+
+    #printe o grafico no tempo do sinal a ser reproduzido
+    plt.plot(tb_y, tone)
+    plt.title("Gráfico de sinal por tempo")
+
+    # reproduz o som
+    sd.play(tone, freq_amostra)
+
+    # Exibe gráficos
+    plt.show()
+    # aguarda fim do audio
+    sd.wait()
+
+if __name__ == "__main__":
+    main()
